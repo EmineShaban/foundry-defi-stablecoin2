1afc8d0a71069a1af7ab2f05c9583f9b195273c5
Emine Shaban <emineshaban@icloud.com>
2025-02-17 12:51:11 +0200

    DecentralizedStableCoin test

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,3 +1,36 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
+
+import {ERC20, ERC20Burnable} from "lib/openzeppelin-contracts/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    constructor(address initialOwner) ERC20("DecentralizedStableCoin", "DSC") Ownable(initialOwner) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedBalance();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
---
+++ test/DecentralizedStableCoinTest.t.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {console} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
+import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {StdCheats} from "forge-std/StdCheats.sol";
+
+contract DecentralizedStableCoinTest is Test {
+    DecentralizedStableCoin dsc;
+
+    function setUp() public {
+        dsc = new DecentralizedStableCoin(msg.sender);
+    }
+
+    function testMustMintMoreThanZero() public {
+        vm.prank(dsc.owner());
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__MustBeMoreThanZero.selector);
+        dsc.mint(address(this), 0);
+    }
+
+ function testMustBurnMoreThanZero() public {
+    address owner = dsc.owner(); // Get the contract owner
+    vm.prank(owner); // Act as the owner
+    dsc.mint(owner, 100); // Mint tokens to the owner
+
+    vm.prank(owner); // Act as the owner again for burning
+    vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__MustBeMoreThanZero.selector);
+    dsc.burn(0); // Attempt to burn 0 tokens (should revert)
+}
+
+
+    function testCantBurnMoreThanYouHave() public {
+        address owner = dsc.owner(); // Get the contract owner
+    vm.prank(owner); // Act as the owner
+    dsc.mint(owner, 100); // Mint tokens to the owner
+
+    vm.prank(owner); // Act as the owner again for burning
+    vm.expectRevert();
+    dsc.burn(1000); // Attempt to burn 0 tokens (should revert)
+    }
+    function testCantMintToZeroAddress() public {
+           vm.prank(dsc.owner());
+        vm.expectRevert(DecentralizedStableCoin.DecentralizedStableCoin__NotZeroAddress.selector);
+        dsc.mint(address(0), 100);
+    }
+}
