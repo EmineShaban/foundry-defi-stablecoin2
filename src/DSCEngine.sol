993ea8cda957e9391613ccc9852ccb3597f03e29
Emine Shaban <emineshaban@icloud.com>
2025-02-17 13:12:09 +0200

    Project setup - DSCEngine

---
+++ src/DSCEngine.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+
+contract DSCEngine {
+
+///////////////////
+//   Functions   //
+///////////////////
+
+///////////////////////////
+//   External Functions  //
+///////////////////////////
+    function depositCollateralAndMintDsc() external {}
+
+    function depositCollateral() external {}
+
+    function redeemCollateralForDsc() external {}
+
+    function redeemCollateral() external {}
+
+    function mintDsc() external {}
+
+    function burnDsc() external {}
+
+    function liquidate() external {}
+
+    function getHealthFactor() external view {}
+}
