c25bf1225e4aa5051bc1f09f33db72221a613d64
Emine Shaban <emineshaban@icloud.com>
2025-02-18 16:51:20 +0200

    Finish the mint function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -14,12 +14,17 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__TokenAddressesAndPriceFeedAddressesMustBeSameLength();
     error DSCEngine__TokenNotAllowed(address token);
     error DSCEngine__TransferFailed();
+    error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     ///////////////////
     //State Variables//
     ///////////////////
     uint256 private constant PRECISION = 1e18;
     uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant LIQUIDATION_THRESHOLD = 50;
+    uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant MIN_HEALTH_FACTOR = 1;
 
     mapping(address token => address priceFeed) private s_priceFeeds;
     mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
@@ -88,6 +93,11 @@ contract DSCEngine is ReentrancyGuard {
 
     function mintDsc(uint256 amountDscToMint) public moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
+        _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if(!minted){
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
@@ -111,15 +121,22 @@ contract DSCEngine is ReentrancyGuard {
 
     function _healthFactor(address user) private view returns (uint256) {
         (uint256 totalDscMinted, uint256 collateralValueInUsd) = _getAccountInformation(user);
+        uint256 collateralAdjustedForThreshold = (collateralValueInUsd * LIQUIDATION_PRECISION) / LIQUIDATION_PRECISION;
+        return (collateralAdjustedForThreshold * PRECISION) / totalDscMinted;
     }
 
-    function _revertIfHealthFactorIsBroken(address user) internal view {}
+    function _revertIfHealthFactorIsBroken(address user) internal view {
+        uint256 userHealthFactor = _healthFactor(user);
+        if (userHealthFactor < MIN_HEALTH_FACTOR) {
+            revert DSCEngine__BreaksHealthFactor(userHealthFactor);
+        }
+    }
 
     /////////////////////////////////////////
     //  Public & External View Functions   //
     /////////////////////////////////////////
 
-    function getAccountCollateralValue(address user) external view returns (uint256 totalCollateralValueInUsd) {
+    function getAccountCollateralValue(address user) public view returns (uint256 totalCollateralValueInUsd) {
         for (uint256 i = 0; i < s_collateralTokens.length; i++) {
             address token = s_collateralTokens[i];
             uint256 amount = s_collateralDeposited[user][token];
@@ -128,7 +145,7 @@ contract DSCEngine is ReentrancyGuard {
         return totalCollateralValueInUsd;
     }
 
-    function getUsdValue(address token, uint256 amount) external view returns (uint256) {
+    function getUsdValue(address token, uint256 amount) public view returns (uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[token]);
         (, int256 price,,,) = priceFeed.latestRoundData();
         return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
--- test/DSCEngineTest.t.sol
+++ test/DSCEngineTest.t.sol
@@ -10,66 +10,66 @@ import {StdCheats} from "forge-std/StdCheats.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DSCEngineTest is Test {
-    // DSCEngine public dsce;
-    // DecentralizedStableCoin public dsc;
-    // HelperConfig public helperConfig;
+// DSCEngine public dsce;
+// DecentralizedStableCoin public dsc;
+// HelperConfig public helperConfig;
 
-    // address public ethUsdPriceFeed;
-    // address public btcUsdPriceFeed;
-    // address public weth;
-    // address public wbtc;
-    // uint256 public deployerKey;
+// address public ethUsdPriceFeed;
+// address public btcUsdPriceFeed;
+// address public weth;
+// address public wbtc;
+// uint256 public deployerKey;
 
-    // uint256 amountCollateral = 10 ether;
-    // uint256 amountToMint = 100 ether;
-    // address public user = address(1);
+// uint256 amountCollateral = 10 ether;
+// uint256 amountToMint = 100 ether;
+// address public user = address(1);
 
-    // uint256 public constant STARTING_USER_BALANCE = 10 ether;
-    // uint256 public constant MIN_HEALTH_FACTOR = 1e18;
-    // uint256 public constant LIQUIDATION_THRESHOLD = 50;
+// uint256 public constant STARTING_USER_BALANCE = 10 ether;
+// uint256 public constant MIN_HEALTH_FACTOR = 1e18;
+// uint256 public constant LIQUIDATION_THRESHOLD = 50;
 
-    // function setUp() external {
-    //     DeployDSC deployer = new DeployDSC();
-    //     (dsc, dsce, helperConfig) = deployer.run();
-    //     (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
-    //     if (block.chainid == 31_337) {
-    //         vm.deal(user, STARTING_USER_BALANCE);
-    //     }
-    //     // Should we put our integration tests here?
-    //     // else {
-    //     //     user = vm.addr(deployerKey);
-    //     //     ERC20Mock mockErc = new ERC20Mock("MOCK", "MOCK", user, 100e18);
-    //     //     MockV3Aggregator aggregatorMock = new MockV3Aggregator(
-    //     //         helperConfig.DECIMALS(),
-    //     //         helperConfig.ETH_USD_PRICE()
-    //     //     );
-    //     //     vm.etch(weth, address(mockErc).code);
-    //     //     vm.etch(wbtc, address(mockErc).code);
-    //     //     vm.etch(ethUsdPriceFeed, address(aggregatorMock).code);
-    //     //     vm.etch(btcUsdPriceFeed, address(aggregatorMock).code);
-    //     // }
-    //     ERC20Mock(weth).mint(user, STARTING_USER_BALANCE);
-    //     ERC20Mock(wbtc).mint(user, STARTING_USER_BALANCE);
-    // }
-    // ///////////////////////////////////////
-    // // depositCollateral Tests //
-    // ///////////////////////////////////////
+// function setUp() external {
+//     DeployDSC deployer = new DeployDSC();
+//     (dsc, dsce, helperConfig) = deployer.run();
+//     (ethUsdPriceFeed, btcUsdPriceFeed, weth, wbtc, deployerKey) = helperConfig.activeNetworkConfig();
+//     if (block.chainid == 31_337) {
+//         vm.deal(user, STARTING_USER_BALANCE);
+//     }
+//     // Should we put our integration tests here?
+//     // else {
+//     //     user = vm.addr(deployerKey);
+//     //     ERC20Mock mockErc = new ERC20Mock("MOCK", "MOCK", user, 100e18);
+//     //     MockV3Aggregator aggregatorMock = new MockV3Aggregator(
+//     //         helperConfig.DECIMALS(),
+//     //         helperConfig.ETH_USD_PRICE()
+//     //     );
+//     //     vm.etch(weth, address(mockErc).code);
+//     //     vm.etch(wbtc, address(mockErc).code);
+//     //     vm.etch(ethUsdPriceFeed, address(aggregatorMock).code);
+//     //     vm.etch(btcUsdPriceFeed, address(aggregatorMock).code);
+//     // }
+//     ERC20Mock(weth).mint(user, STARTING_USER_BALANCE);
+//     ERC20Mock(wbtc).mint(user, STARTING_USER_BALANCE);
+// }
+// ///////////////////////////////////////
+// // depositCollateral Tests //
+// ///////////////////////////////////////
 
-    // // this test needs it's own setup
-    // function testRevertsIfTransferFromFails() public {
-    //     // Arrange - Setup
-    // }
+// // this test needs it's own setup
+// function testRevertsIfTransferFromFails() public {
+//     // Arrange - Setup
+// }
 
-    // function testRevertsIfCollateralZero() public {
-    //     vm.prank(user);
-    //     ERC20Mock(weth).approve(address(dsce), amountCollateral);
-    // }
+// function testRevertsIfCollateralZero() public {
+//     vm.prank(user);
+//     ERC20Mock(weth).approve(address(dsce), amountCollateral);
+// }
 
-    // function testRevertsWithUnapprovedCollateral() public {}
+// function testRevertsWithUnapprovedCollateral() public {}
 
-    // modifier depositedCollateral() {}
+// modifier depositedCollateral() {}
 
-    // function testCanDepositCollateralWithoutMinting() public depositedCollateral {}
+// function testCanDepositCollateralWithoutMinting() public depositedCollateral {}
 
-    // function testCanDepositedCollateralAndGetAccountInfo() public depositedCollateral {}
+// function testCanDepositedCollateralAndGetAccountInfo() public depositedCollateral {}
 }
